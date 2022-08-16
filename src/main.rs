62561e3ed4ab427a060add0a73407d6b46a534be
Luke Eltiste <luke343279@gmail.com>
2022-08-15 17:42:31 -0700

    refactor: setup basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,63 @@
-use chrono::{DateTime, Utc};
 use std::ffi::OsStr;
-use std::fs;
 use std::path::Path;
-use walkdir::WalkDir;
 
-fn hash(file: &str) -> String {
-    let computed_hash = sha256::digest_file(std::path::Path::new(&file)).unwrap();
-    computed_hash
-}
+use fs_err as fs;
+use itertools::Itertools;
+use walkdir::WalkDir;
 
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
     Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_dir = "./test_files/input";
-    let output_dir = "./test_files/output";
-    //let input_dir = "Z:/data/tmp/input";
-    //let output_dir = "Z:/data/tmp/output";
-    fs::create_dir_all(output_dir)?;
+    let input_dir = "Z:\\NextCloud\\frazzer\\files\\Photos";
+    //let output_dir = "Z:\\NextCloud\\frazzer\\files\\Picture_Processing";
+    let output_photo_dir = "Z:\\NextCloud\\frazzer\\files\\Picture_Processing\\pictures";
+    let output_video_dir = "Z:\\NextCloud\\frazzer\\files\\Picture_Processing\\videos";
+    let output_other_dir = "Z:\\NextCloud\\frazzer\\files\\Picture_Processing\\other";
+
+    fs::create_dir_all(output_photo_dir)?;
+    fs::create_dir_all(output_video_dir)?;
+    fs::create_dir_all(output_other_dir)?;
+
+    let photo_extensions = vec!["png", "jpg", "heic"];
+    let video_extensions = vec!["mp4", "mov"];
+    let other_extensions = vec!["gif"];
+    let skip_extensions = vec!["md"];
+    let mut unknown_extensions = vec![];
 
     for entry in WalkDir::new(input_dir).into_iter().filter_map(|e| e.ok()) {
         if entry.metadata().unwrap().is_file() {
             let path = entry.path();
-            println!("{}", path.display());
+            //println!("{}", path.display());
 
-            if let Ok(time) = entry.metadata().unwrap().created() {
-                let date = DateTime::<Utc>::from(time).date();
-                let year = date.format("%Y").to_string();
-                let month = date.format("%m").to_string();
-                let day = date.format("%d").to_string();
-                let hash = hash(path.to_str().unwrap());
-                let extension =
-                    get_extension_from_filename(path.to_str().unwrap()).unwrap_or("missing");
+            let extention =
+                get_extension_from_filename(path.to_str().unwrap()).unwrap_or("missing").to_lowercase();
 
-                //println!("{year}-{month}-{day}");
-                //println!("{hash}");
-
-                fs::create_dir_all(std::format!("{output_dir}/{year}/{month}/{day}"))?;
-
-                fs::rename(
-                    path,
-                    std::format!(
-                        "{output_dir}/{year}/{month}/{day}/{year}-{month}-{day}_{hash}.{extension}"
-                    ),
-                )?;
+            if photo_extensions.contains(&extention.as_str()) {
+                println!("Photo:   {}", path.display());
+            } else if video_extensions.contains(&extention.as_str()) {
+                println!("Video:   {}", path.display());
+            } else if other_extensions.contains(&extention.as_str()) {
+                println!("Other:   {}", path.display());
+            } else if skip_extensions.contains(&extention.as_str()) {
+                println!("Skip:    {}", path.display());
             } else {
-                println!("Not supported on this platform or filesystem");
+                println!("Unknown: {}", path.display());
+                unknown_extensions.push(extention.to_string());
             }
+
+            //    fs::rename(
+            //        path,
+            //        std::format!(
+            //            "{output_dir}/{year}/{month}/{day}/{year}-{month}-{day}_{hash}.{extension}"
+            //        ),
+            //    )?;
         }
     }
+
+    let unknown_extensions: Vec<_> = unknown_extensions.into_iter().unique().collect();
+    println!("Unknown Extension: {:#?}", unknown_extensions);
 
     Ok(())
 }
